11a448f5a857d87849c59c428a754c9d7a64c7c0
GerardAguilar <gerard.aguilar.156@my.csun.edu>
2016-12-10 19:31:41 -0800

    Animation Blend Tree

--- SurvivalShooterJumperSlasher/Assets/Scripts/Player/PlayerMovement.cs
+++ SurvivalShooterJumperSlasher/Assets/Scripts/Player/PlayerMovement.cs
@@ -19,6 +19,11 @@ public class PlayerMovement : MonoBehaviour
     void FixedUpdate() {
         float h = Input.GetAxisRaw("Horizontal"); //just -1,0,1 instead of -1...-0.5...0...0.5...1
         float v = Input.GetAxisRaw("Vertical");
+
+        Move(h, v);
+        Turning();
+        Animating(h, v);
+
     }
 
     void Move(float h, float v) {
@@ -29,5 +34,22 @@ public class PlayerMovement : MonoBehaviour
 
     void Turning() {
         //Finished at 26:57
+        Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit floorHit;
+
+        if (Physics.Raycast(camRay, out floorHit, camRayLength, floorMask)) {
+            Vector3 playerToMouse = floorHit.point - transform.position;
+            playerToMouse.y = 0f; //flatten the playerToMouse onto the floor
+
+            Quaternion newRotation = Quaternion.LookRotation(playerToMouse);//z-axis is by default the forward vector
+            playerRigidbody.MoveRotation(newRotation);
+        }
+    }
+
+    void Animating(float h, float v) {
+        bool walking = h!= 0f || v != 0f;//senses movement in the horizontal or the vertical
+        anim.SetBool("IsWalking", walking);
+        //issues with animating. 09DEC2016 8:55am
     }
 }
